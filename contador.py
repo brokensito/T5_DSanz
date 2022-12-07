e4771fbc309853563a23d3a5b935b467256f35fd
David Sanz <dsanzfuertes.15@gmail.com>
2022-12-07 14:22:27 +0100

    terminar ej 2

--- contador.py
+++ contador.py
@@ -1,8 +1,30 @@
-archivo = open("hola.txt")
+import sys
 
-try:
-    abrir = open("hola.txt")
+archivo = open("contador.txt","a+")
+archivo.seek(0)
 
-except:
+contador = archivo.readline()
 
-    
+if len(contador)==0:
+    datos = "0"
+    archivo.write(datos)
+
+else:
+    try:
+        datos = int(contador)
+        if len(sys.argv)==2:
+            if sys.argv[1]=="inc":
+                datos +=1
+
+            elif sys.argv[1]=="dec":
+                datos -=1
+        archivo = open("contador.txt","w")
+        archivo.write(str(datos))
+
+    except IOError:
+        print("Error: fichero corrupto.")
+
+archivo.close()
+
+
+
